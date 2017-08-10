144eedb0257bff377d964243b9902aaab6a8c927
Michael Englert <michael.englert@osxltmengl.local>
2017-08-10 16:47:01 +0200

    added main

---
+++ src/main/java/com/appdynamics/api/appcheck/CheckApplications.java
@@ -0,0 +1,14 @@
+package com.appdynamics.api.appcheck;
+
+public class CheckApplications {
+    public static void main(String[] args)
+    {
+        try
+        {
+
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
